8215a347b7e3195f91740a6b183a153615c42747
tomas <portatomas55@gmail.com>
2023-07-11 19:57:18 -0300

    Agregar archivos de proyecto.

---
+++ parcial tm2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace parcial_tm2
+{
+    internal class Program
+    {
+        private static double l;
+
+        static void Main(string[] args)
+        {
+            int  l, l2;
+            double h,arista;
+            Console.Write("ingrese un lado");
+            l=int.Parse(Console.ReadLine());
+            Console.Write(" ingrese otro lado");
+            l2 = int.Parse(Console.ReadLine());
+
+            var area=calculararea(h,l);
+             var volumen=calcularvolumen(h,l2);   
+        }
+
+        private static object calcularvolumen(double h, int l2)
+        {
+            return (Math.PI * Math.Pow(l, 2) * h) / 3;
+        }
+
+        private static object calculararea(double h, int l)
+        {
+           return l*(l+Math.Sqrt(4))
+        }
+    }
+}
